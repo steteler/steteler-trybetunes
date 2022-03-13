757ec727134928eeb8f9a14b6b004df1b49ee1c0
kikutii <joaovictorkikuti@gmail.com>
2022-03-13 19:28:01 -0300

    terminado 5 requisito, adicionado input e button para pesquisa

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -2,11 +2,51 @@ import React, { Component } from 'react';
 import Header from '../components/Header';
 
 class Search extends Component {
+  constructor() {
+    super();
+    this.state = {
+      inputSearch: '',
+      isButtonDisabled: true,
+    };
+  }
+
+  onInputChange = ({ target: { value } }) => {
+    this.setState(() => ({
+      inputSearch: value,
+    }), this.changeButtonDisabled);
+  }
+
+  changeButtonDisabled = () => {
+    const { inputSearch } = this.state;
+    const minCharacter = 2;
+    this.setState(() => ({
+      isButtonDisabled: inputSearch.length < minCharacter,
+    }));
+  }
+
   render() {
+    const {
+      inputSearch,
+      isButtonDisabled,
+    } = this.state;
+
     return (
       <div data-testid="page-search">
         <Header />
         Search
+        <input
+          data-testid="search-artist-input"
+          type="text"
+          value={ inputSearch }
+          onChange={ this.onInputChange }
+        />
+        <button
+          data-testid="search-artist-button"
+          type="submit"
+          disabled={ isButtonDisabled }
+        >
+          Procurar
+        </button>
       </div>
     );
   }
