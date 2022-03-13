c1b2f1ff8dfb6b0f41990a460632104345b4b535
kikutii <joaovictorkikuti@gmail.com>
2022-03-13 17:15:49 -0300

    refatorado componente Loading

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -1,11 +1,20 @@
 import React, { Component } from 'react';
+import PropType from 'prop-types';
 
 class Loading extends Component {
   render() {
+    const { isLoading } = this.props;
+
     return (
-      <h1>Carregando...</h1>
+      <div>
+        {isLoading && <h1>Carregando...</h1>}
+      </div>
     );
   }
 }
+
+Loading.propTypes = {
+  isLoading: PropType.bool.isRequired,
+};
 
 export default Loading;
