190bc0e4539d2adeadbe7c35146612908a3ec02f
kikutii <joaovictorkikuti@gmail.com>
2022-03-14 19:10:25 -0300

    terminado requisito 6, adicionado cards

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
+import Card from '../components/Card';
 import Header from '../components/Header';
+import Loading from '../components/Loading';
+import searchAlbumsAPI from '../services/searchAlbumsAPI';
 
 class Search extends Component {
   constructor() {
@@ -7,6 +10,9 @@ class Search extends Component {
     this.state = {
       inputSearch: '',
       isButtonDisabled: true,
+      isLoading: false,
+      artistName: '',
+      album: [],
     };
   }
 
@@ -24,29 +30,56 @@ class Search extends Component {
     }));
   }
 
+  onHandleClick = () => {
+    const { inputSearch } = this.state;
+    this.setState(() => ({
+      isLoading: true,
+      artistName: inputSearch,
+      inputSearch: '',
+    }), () => (
+      searchAlbumsAPI(inputSearch)
+        .then((album) => (
+          this.setState(() => ({ isLoading: false, album }))
+        ))));
+  }
+
   render() {
     const {
       inputSearch,
       isButtonDisabled,
+      isLoading,
+      artistName,
+      album,
     } = this.state;
 
     return (
       <div data-testid="page-search">
         <Header />
         Search
-        <input
-          data-testid="search-artist-input"
-          type="text"
-          value={ inputSearch }
-          onChange={ this.onInputChange }
-        />
-        <button
-          data-testid="search-artist-button"
-          type="submit"
-          disabled={ isButtonDisabled }
-        >
-          Procurar
-        </button>
+        {isLoading ? <Loading isLoading={ isLoading } /> : (
+          <div>
+            <input
+              data-testid="search-artist-input"
+              type="text"
+              value={ inputSearch }
+              onChange={ this.onInputChange }
+            />
+            <button
+              data-testid="search-artist-button"
+              type="submit"
+              disabled={ isButtonDisabled }
+              onClick={ this.onHandleClick }
+            >
+              Procurar
+            </button>
+          </div>
+        )}
+        {artistName && <h3>{ `Resultado de álbuns de: ${artistName}` }</h3>}
+        {!album.length ? <p>Nenhum álbum foi encontrado</p> : (
+          album.map((artist) => (
+            <Card key={ artist.collectionId } { ...artist } />
+          ))
+        )}
       </div>
     );
   }
