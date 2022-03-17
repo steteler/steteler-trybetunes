91909264095dc4703e9a2bd31e454ac7f83ebc75
kikutii <joaovictorkikuti@gmail.com>
2022-03-17 12:39:37 -0300

    terminado requisito 13, adicionado perfil

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,12 +1,50 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import Header from '../components/Header';
+import { getUser } from '../services/userAPI';
+import Loading from '../components/Loading';
 
 class Profile extends Component {
+  constructor() {
+    super();
+    this.state = {
+      isLoading: false,
+      name: '',
+      email: '',
+      description: '',
+      image: '',
+    };
+  }
+
+  componentDidMount = () => {
+    this.setState(() => ({ isLoading: true }), () => {
+      getUser().then(({ name, email, description, image }) => {
+        this.setState(() => ({
+          name,
+          email,
+          description,
+          image,
+          isLoading: false,
+        }));
+      });
+    });
+  }
+
   render() {
+    const { name, email, description, image, isLoading } = this.state;
+
     return (
       <div data-testid="page-profile">
         <Header />
-        Profile
+        {isLoading ? <Loading isLoading={ isLoading } /> : (
+          <>
+            <p>{ name }</p>
+            <p>{ email }</p>
+            <p>{ description }</p>
+            <img data-testid="profile-image" src={ image } alt={ name } />
+            <Link to="/profile/edit">Editar perfil</Link>
+          </>
+        )}
       </div>
     );
   }
