acc5be8ea6e856a7cd98902432eb27a82c6e7f23
kikutii <joaovictorkikuti@gmail.com>
2022-03-16 21:47:39 -0300

    terminado requisito 12, adicionado page favoritos

--- src/pages/Favorites.js
+++ src/pages/Favorites.js
@@ -1,12 +1,41 @@
 import React, { Component } from 'react';
 import Header from '../components/Header';
+import MusicCard from '../components/MusicCard';
+import { getFavoriteSongs } from '../services/favoriteSongsAPI';
 
 class Favorites extends Component {
+  constructor() {
+    super();
+    this.state = {
+      favoriteSongs: [],
+    };
+  }
+
+  updateFavorites = () => {
+    getFavoriteSongs().then((favorites) => {
+      this.setState(() => ({ favoriteSongs: [...favorites] }));
+    });
+  }
+
+  componentDidMount = () => {
+    this.updateFavorites();
+  }
+
   render() {
+    const { favoriteSongs } = this.state;
     return (
       <div data-testid="page-favorites">
         <Header />
         Favorites
+        {
+          favoriteSongs.map((favorite) => (
+            <MusicCard
+              key={ favorite.trackId }
+              album={ favorite }
+              updateFavorites={ this.updateFavorites }
+            />
+          ))
+        }
       </div>
     );
   }
